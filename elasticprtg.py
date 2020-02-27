98b09eb9e40ecdfc5142d69eeecbb20a4ad3ea6c
B0nk3rZ <b0nk3rz@b0nk3rz.net>
2020-02-27 11:42:00 +0100

    Updated for PRTG versions >20.1.55

--- elasticprtg.py
+++ elasticprtg.py
@@ -1,46 +1,53 @@
 import requests
 import json
 import sys
-from paepy.ChannelDefinition import CustomSensorResult
+from prtg.sensor.result import CustomSensorResult
+from prtg.sensor.units import ValueUnit
 
 
 if __name__ == "__main__":
-	#load the prtg parameters into a dictionary and make call to ES host
-	data = json.loads(sys.argv[1])
-	url = 'http://{0}:9200/_cluster/health'.format(data['host'])
-	req = requests.get(url)
-	
-	#load the json response into a dictionary
-	output = json.loads(req.text)
-	
-	#Turn status colour into a number so that it can be viewed in prtg
-	if output['status'] == 'green':
-		status = 0
-	elif output['status'] == 'yellow':
-		status = 1
-	elif output['status'] == 'red':
-		status = 2
-	
-	#turn timed out into a number so that it can be viewed in prtg
-	if output['timed_out'] == False:
-		timedOut = 0
-	else:
-		timedOut = 1
+	try:
+		#load the prtg parameters into a dictionary and make call to ES host
+		data = json.loads(sys.argv[1])
+		url = 'http://{0}:9200/_cluster/health'.format(data['host'])
+		req = requests.get(url)
 		
-	#create a prtg sensor and add the channel data. Each key/value in the json response from ES gets channelised
-	sensor = CustomSensorResult(output['status'])
-	sensor.add_channel(channel_name="Status",unit="Count",value=status,is_limit_mode=True,limit_max_error=1.5,limit_max_warning=0.5)
-	sensor.add_channel(channel_name="Number Of Data Nodes",unit="Count",value=output['number_of_data_nodes'])
-	sensor.add_channel(channel_name="Number of Nodes",unit="Count",value=output['number_of_nodes'])
-	sensor.add_channel(channel_name="Percent of Shards Active",unit="Percent",value=output['active_shards_percent_as_number'])
-	sensor.add_channel(channel_name="Delayed Unassigned Shards",unit="Count",value=output['delayed_unassigned_shards'])
-	sensor.add_channel(channel_name="In Flight Fetches",unit="Count",value=output['number_of_in_flight_fetch'])
-	sensor.add_channel(channel_name="Relocating Shards",unit="Count",value=output['relocating_shards'])
-	sensor.add_channel(channel_name="Pending Tasks",unit="Count",value=output['number_of_pending_tasks'])
-	sensor.add_channel(channel_name="Initializing Shards",unit="Count",value=output['initializing_shards'])
-	sensor.add_channel(channel_name="Unassigned Shards",unit="Count",value=output['unassigned_shards'])
-	sensor.add_channel(channel_name="Task Queue Time (max)",unit="Milliseconds",value=output['task_max_waiting_in_queue_millis'])
-	sensor.add_channel(channel_name="Timed Out",unit="Count",value=timedOut,is_limit_mode=True,limit_max_error=0.5,limit_error_msg="Timed Out = True")
-	
-	#send the sensor data back to the prtg process in json format
-	print(sensor.get_json_result())
+		#load the json response into a dictionary
+		output = json.loads(req.text)
+		
+		#Turn status colour into a number so that it can be viewed in prtg
+		if output['status'] == 'green':
+			status = 0
+		elif output['status'] == 'yellow':
+			status = 1
+		elif output['status'] == 'red':
+			status = 2
+		
+		#turn timed out into a number so that it can be viewed in prtg
+		if output['timed_out'] == False:
+			timedOut = 0
+		else:
+			timedOut = 1
+			
+		#create a prtg sensor and add the channel data. Each key/value in the json response from ES gets channelised
+		sensor = CustomSensorResult(output['status'])
+		sensor.add_channel(name="Status",unit=ValueUnit.COUNT,value=status,is_limit_mode=True,limit_max_error=1.5,limit_max_warning=0.5)
+		sensor.add_channel(name="Number Of Data Nodes",unit=ValueUnit.COUNT,value=output['number_of_data_nodes'])
+		sensor.add_channel(name="Number of Nodes",unit=ValueUnit.COUNT,value=output['number_of_nodes'])
+		sensor.add_channel(name="Percent of Shards Active",unit=ValueUnit.PERCENT,value=output['active_shards_percent_as_number'])
+		sensor.add_channel(name="Delayed Unassigned Shards",unit=ValueUnit.COUNT,value=output['delayed_unassigned_shards'])
+		sensor.add_channel(name="In Flight Fetches",unit=ValueUnit.COUNT,value=output['number_of_in_flight_fetch'])
+		sensor.add_channel(name="Relocating Shards",unit=ValueUnit.COUNT,value=output['relocating_shards'])
+		sensor.add_channel(name="Pending Tasks",unit=ValueUnit.COUNT,value=output['number_of_pending_tasks'])
+		sensor.add_channel(name="Initializing Shards",unit=ValueUnit.COUNT,value=output['initializing_shards'])
+		sensor.add_channel(name="Unassigned Shards",unit=ValueUnit.COUNT,value=output['unassigned_shards'])
+		sensor.add_channel(name="Task Queue Time (max)",unit="Milliseconds",value=output['task_max_waiting_in_queue_millis'])
+		sensor.add_channel(name="Timed Out",unit=ValueUnit.COUNT,value=timedOut,is_limit_mode=True,limit_max_error=0.5,limit_error_msg="Timed Out = True")
+		
+		#send the sensor data back to the prtg process in json format
+		print(sensor.json_result)
+	except Exception as e:
+		csr = CustomSensorResult(text="Python Script execution error")
+		csr.error = "Python Script execution error: %s" % str(e)
+		print(csr.json_result)
+
--- kibanaprtg.py
+++ kibanaprtg.py
@@ -1,35 +1,41 @@
 import requests
 import json
 import sys
-from paepy.ChannelDefinition import CustomSensorResult
-
+from prtg.sensor.result import CustomSensorResult
+from prtg.sensor.units import ValueUnit
 
 if __name__ == "__main__":
-	#load the prtg parameters into a dictionary and make call to ES host
-	data = json.loads(sys.argv[1])
-	url = 'http://{0}:5601/api/status'.format(data['host'])
-	req = requests.get(url)
+	try:
+		#load the prtg parameters into a dictionary and make call to ES host
+		data = json.loads(sys.argv[1])
+		url = 'http://{0}:5601/api/status'.format(data['host'])
+		req = requests.get(url)
+		
+		#load the json response into a dictionary
+		output = json.loads(req.text)
+		
+		#Turn status colour into a number so that it can be viewed in prtg
+		color = output['status']['overall']['state']
+		if color == 'green':
+			status = 0
+		elif color == 'yellow':
+			status = 1
+		elif color == 'red':
+			status = 2
 	
-	#load the json response into a dictionary
-	output = json.loads(req.text)
 	
-    #Turn status colour into a number so that it can be viewed in prtg
-	color = output['status']['overall']['state']
-	if color == 'green':
-		status = 0
-	elif color == 'yellow':
-		status = 1
-	elif color == 'red':
-		status = 2
+		# create a prtg sensor and add the channel data. Each key/value in the json response from ES gets channelised
+		sensor = CustomSensorResult(output['status']['overall']['state'])
+		sensor.add_channel(channel_name="Status", unit=ValueUnit.COUNT, value=status, is_limit_mode=True, limit_max_error=1.5,limit_max_warning=0.5)
+		sensor.add_channel(channel_name="Metrics Requests Total", unit=ValueUnit.COUNT, value=output['metrics']['requests']['total'])
+		sensor.add_channel(channel_name="Metrics Connections", unit=ValueUnit.COUNT, value=output['metrics']['concurrent_connections'])
+		sensor.add_channel(channel_name="Max. Response Time", unit="Milliseconds", value=output['metrics']['response_times']['max_in_millis'])
+		sensor.add_channel(channel_name="Avg. Response Time", unit="Milliseconds", value=output['metrics']['response_times']['avg_in_millis'])
+	
+		# send the sensor data back to the prtg process in json format
+		print(sensor.json_result)
+	except Exception as e:
+		csr = CustomSensorResult(text="Python Script execution error")
+		csr.error = "Python Script execution error: %s" % str(e)
+		print(csr.json_result)
 
-
-    # create a prtg sensor and add the channel data. Each key/value in the json response from ES gets channelised
-	sensor = CustomSensorResult(output['status']['overall']['state'])
-	sensor.add_channel(channel_name="Status", unit="Count", value=status, is_limit_mode=True, limit_max_error=1.5,limit_max_warning=0.5)
-	sensor.add_channel(channel_name="Metrics Requests Total", unit="Count", value=output['metrics']['requests']['total'])
-	sensor.add_channel(channel_name="Metrics Connections", unit="Count", value=output['metrics']['concurrent_connections'])
-	sensor.add_channel(channel_name="Max. Response Time", unit="Milliseconds", value=output['metrics']['response_times']['max_in_millis'])
-	sensor.add_channel(channel_name="Avg. Response Time", unit="Milliseconds", value=output['metrics']['response_times']['avg_in_millis'])
-
-    # send the sensor data back to the prtg process in json format
-	print(sensor.get_json_result())
